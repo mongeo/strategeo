9697243e7a9c43ada5e3633aaaaa1db430c83d18
Geoffrey Montague <gmontague0205@gmail.com>
2015-06-01 17:58:28 -0700

    Worked on database connection

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
-$path = $_SERVER['CONTEXT_DOCUMENT_ROOT'] . "/stratego/";
+ini_set('display_errors', 1);
 
-if (isset($_COOKIE['name']) || isset($_SESSION['name'])){
-   header("Location: user/welcome.php");
-   exit();
+$path = $_SERVER['CONTEXT_DOCUMENT_ROOT'] . "/strategeo/";
+
+if(function_exists('mysqli_connect')){
+	echo "EXISTS!";
 } else {
-   header("Location: user/login.php");
+  echo "Nope";
 }
 
+
+$conn = "";
+$file = "../../conf/db.conn";
+if($f = fopen($file, 'r')){
+   $line = fgets($f);
+   $fields = explode(' ', $line);
+   $conn = mysqli_connect($fields[0],$fields[1],$fields[2],$fields[3]) or die(mysqli_error());
+   fclose($f);
+} else {
+   echo "Could not read configuration file: $file . to connect ot database";
+   exit();
+}
+
+#if (isset($_COOKIE['name']) || isset($_SESSION['name'])){
+#   header("Location: user/welcome.php");
+#   exit();
+#} else {
+#   header("Location: user/login.php");
+#}
+echo $path;
 ?>
